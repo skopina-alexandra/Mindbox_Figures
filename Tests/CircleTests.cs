d33b938b20d33ddd8c41474801c0023a1e99d17f
Alexandra Skopina <skopina.aa@outlook.com>
2022-09-08 23:58:21 +1000

    new tests added

--- Tests/CircleTests.cs
+++ Tests/CircleTests.cs
@@ -29,13 +29,19 @@ namespace Tests
                 Assert.IsInstanceOfType(e, typeof(System.ArgumentException));
             }
 
-           
         }
 
         [TestMethod]
         public void TestConstructor_WithPositiveRadius()
         {
             Assert.IsInstanceOfType(new Circle(2.83), typeof(Circle));
+        }
+
+        [TestMethod]
+        public void Test_Area()
+        {
+            Circle circle = new Circle(5);
+            Assert.AreEqual(circle.Area(), 78.53981633974483);
         }
     }
 }
--- Tests/TriangleTests.cs
+++ Tests/TriangleTests.cs
@@ -21,5 +21,26 @@ namespace Tests
         {
             Assert.IsInstanceOfType(new Triangle(10, 3, 8), typeof(Triangle));
         }
+
+        [TestMethod]
+        public void Test_TriangleIsRight()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            Assert.IsTrue(triangle.IsRight());
+        }
+
+        [TestMethod]
+        public void Test_TriangleIsNotRight()
+        {
+            Triangle triangle = new Triangle(3, 4, 6);
+            Assert.IsFalse(triangle.IsRight());
+        }
+
+        [TestMethod]
+        public void Test_Area()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            Assert.AreEqual(triangle.Area(), 6);
+        }
     }
 }
